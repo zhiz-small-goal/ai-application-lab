1bbf0ca26a5f4727674e49d12de720b3dc7ef5c8
zhiz-small-goal <zhiz2024520@gmail.com>
2026-07-28 17:02:42 +0800

    feat(api): 新增 api
    新增网络调用模块, 可以从网页中对 run_file_processing() 进行调用, 类似远程进行指定的文件处理
    
    - 有个小问题, 刚刚我拼写错误, 错误写法"in None", 应该写成"is None".
    错误信息是"TypeError: argument of type 'NoneType' is not iterable"
    以后就知道这个错误是什么意思了.
    - 生成的文件放在 input 目录了, 待会检查一下怎么回事

---
+++ api.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+from fastapi import FastAPI, HTTPException
+from pydantic import  BaseModel
+
+from main import run_file_processing
+from models import FileRecord
+
+
+app = FastAPI(
+    title="File Processing API",
+)
+
+
+class ProcessFileRequest(BaseModel):
+    input_dir: Path
+    output_dir: Path
+
+
+class ProcessFileResponse(BaseModel):
+    total: int
+    records: list[FileRecord]
+
+
+@app.get("/health")
+def health_check() -> dict[str, str]:
+    return {
+        "status": "ok",
+    }
+
+
+@app.post(
+    "/process",
+    response_model=ProcessFileResponse,
+)
+def process_files(
+    request: ProcessFileRequest,
+) -> ProcessFileResponse:
+    records = run_file_processing(
+        input_dir=request.input_dir,
+        output_dir=request.input_dir,
+    )
+
+    if records is None:
+        raise HTTPException(
+            status_code=400,
+            detail=(
+                "Input directory does not exist"
+                "or is not directory"
+            ),
+        )
+
+    return ProcessFileResponse(
+        total=len(records),
+        records=records,
+    )
