da7c9a3e9ef57e3af1dd222ecf114403249f6c6d
zhiz-small-goal <zhiz2024520@gmail.com>
2026-08-20 12:04:17 +0800

    refactor(smoke): 打印看看是哪个 evidence 出现多个，出现多少个

--- cross_parser_mapping_smoke.py
+++ cross_parser_mapping_smoke.py
@@ -30,31 +30,53 @@ parser_text = extract(
 )
 
 
-evidence_text = "项目预算：60万元"
-
-assert reference_text.count(evidence_text) == 1
-
-reference_start = reference_text.index(evidence_text)
-reference_end = reference_start + len(evidence_text)
-
-print(reference_start, reference_end)
-print(reference_text[reference_start:reference_end])
-
-
-parser_position = project_evidence_span(
-    reference_text=reference_text,
-    reference_start=reference_start,
-    reference_end=reference_end,
-    parser_text=parser_text,
-)
-
-assert parser_position is not None
-
-parser_start, parser_end = parser_position
-
-projected_text = parser_text[
-    parser_start:parser_end
+evidences = [
+    "湖南银行2025人工智能管理平台研发服务项目",
+    "公开招标采购",
+    "项目预算：60万元",
+    "供应商参与投标",
 ]
 
-print("parser position: ", parser_position)
-print("projected text: ", projected_text)
+
+reference_position = []
+
+for evidence_text in evidences:
+
+    count = reference_text.count(evidence_text)
+
+    print(
+        "evidence: ",
+        evidence_text,
+        "count: ",
+        count
+    )
+
+    assert count == 1
+
+    reference_start = reference_text.index(evidence_text)
+    reference_end = reference_start + len(evidence_text)
+
+    print(reference_start, reference_end)
+    print(reference_text[reference_start:reference_end])
+    reference_position.append((reference_start, reference_end))
+
+for reference_offsets in reference_position:
+    reference_start, reference_end = reference_offsets
+
+    parser_position = project_evidence_span(
+        reference_text=reference_text,
+        reference_start=reference_start,
+        reference_end=reference_end,
+        parser_text=parser_text,
+    )
+
+    assert parser_position is not None
+
+    parser_start, parser_end = parser_position
+
+    projected_text = parser_text[
+        parser_start:parser_end
+    ]
+
+    print("parser position: ", parser_position)
+    print("projected text: ", projected_text)
