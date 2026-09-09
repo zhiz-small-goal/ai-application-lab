0933cab388b3cedbafbd953ce9c2a4acef47b489
zhiz-small-goal <zhiz2024520@gmail.com>
2026-09-09 17:31:01 +0800

    feat(web_retry)：给 web_tests.py 的网络信息获取中不同的错误情况进行适当的 Retry 次数

--- web_tests.py
+++ web_tests.py
@@ -1,22 +1,43 @@
+import time
+
 from httpx import Client
 
 
 url = "https://api.github.com/repos/python/cpython"
 
 with Client() as client:
+    
+    MAX_RETRIES = 3
+    retry_count = 0
 
-    response = client.get(url)
+    while True:
+        if retry_count == MAX_RETRIES:
+            break
+        response = client.get(url)
 
-    print("\n")
-    print(response.status_code)
-    print("\n")
-    print(response.headers.get("x-ratelimit-remaining"))
+        status_code = response.status_code
 
-    data = response.json()
+        if status_code == 200:
+            print(response.json())
+            break
 
-    print(
-        "\n",
-        data["name"],
-        "\n",
-        data["full_name"]
-    )
+        elif status_code == 429:
+            print("Rate Limited")
+            retry_count += 1
+            time.sleep(10)
+            continue
+
+        elif status_code == 403:
+            print("Forbidden")
+            break
+
+        elif status_code in {500, 502, 503, 504}:
+            print("Server Error")
+            time.sleep(2)
+            retry_count += 1
+            continue
+
+        else:
+            print("Unknown Status")
+            break
+
