f4f4f448acc62b3067f0e6662524017128a3f724
zhiz-small-goal <zhiz2024520@gmail.com>
2026-09-13 18:12:32 +0800

    feat(scrapy-quotes)：新增 next_page，实现一个 url /Requset 的 Response，产生更多的 Request
    - 其中我传入的第二行 url 提示重复，被过滤掉

--- scrapy_lab/acquisition_lab/acquisition_lab/spiders/quotes_spider.py
+++ scrapy_lab/acquisition_lab/acquisition_lab/spiders/quotes_spider.py
@@ -33,3 +33,13 @@ class QuotesSpider(scrapy.Spider):
             "Save file %s",
             filename
         )
+
+        next_page = response.css(
+            "li.next a::attr(href)"
+        ).get()
+
+        if next_page is not None:
+            yield response.follow(
+                next_page,
+                callback=self.parse,
+            )
