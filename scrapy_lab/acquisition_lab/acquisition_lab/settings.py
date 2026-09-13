aea9ca3b8d10a31e2d16d02e80f85913b8daa20d
zhiz-small-goal <zhiz2024520@gmail.com>
2026-09-13 16:58:37 +0800

    chore(scrapy-settings)：修改
    TELNETCONSOLE_ENABLED = False
    REMOTE_CONTROL_ENABLED = False

--- scrapy_lab/acquisition_lab/acquisition_lab/settings.py
+++ scrapy_lab/acquisition_lab/acquisition_lab/settings.py
@@ -30,7 +30,8 @@ DOWNLOAD_DELAY = 1
 #COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
+REMOTE_CONTROL_ENABLED = False
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
