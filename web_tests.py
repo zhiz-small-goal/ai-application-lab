a3851b812d3deffb7ffae7c7d48c4ca49a806615
zhiz-small-goal <zhiz2024520@gmail.com>
2026-09-14 16:26:03 +0800

    chore:测试 scrapy 保存的文件是否能进入 Trafilatura 流程

--- web_tests.py
+++ web_tests.py
@@ -1,74 +1,17 @@
 from pathlib import Path
-import time
 
-from trafilatura import extract
-
-from httpx import Client
+from html_parser import extract_text_from_html
 
 
-url = "https://api.github.com/repos/python/cpython/readme"
+raw_path = Path("scrapy_lab/acquisition_lab/rfc-rfc.html")
 
-MAX_RETRIES = 3
+html = raw_path.read_text(
+    encoding="utf-8",
+)
+print("HTML size: ", len(html))
 
-raw_path = Path("data/raw/cpython_readme.html")
+parser_text = extract_text_from_html(
+    html=html
+)
 
-with Client(
-    headers={
-        "Accept": "application/vnd.github.html+json"
-    }
-) as client:
-    for attempt in range(MAX_RETRIES + 1):
-        response = client.get(url)
-
-        status_code = response.status_code
-
-        if status_code == 200:
-            raw_html = response.text
-            raw_path.parent.mkdir(
-                parents=True,
-                exist_ok=True,
-            )
-
-            raw_path.write_text(
-                raw_html,
-                encoding="utf-8",
-            )
-
-            print(f"Saved: {raw_path}")
-
-            parser_input_html = (
-                "<!DOCTYPE html>"
-                "<html>"
-                "<body>"
-                f"{raw_html}"
-                "</body>"
-                "</html>"
-            )
-
-            parser_text = extract(parser_input_html)
-
-            if parser_text is None:
-                print("Parser failed")
-                break
-
-            print(parser_text[:1000])
-            break
-
-        elif status_code == 403:
-            print("Forbidden")
-            break
-
-        retryable = (
-            status_code == 429
-            or status_code in {500, 502, 503, 504}
-        )
-
-        if not retryable:
-            print("Unknown Status")
-            break
-
-        if attempt == MAX_RETRIES:
-            print("Max retries reached")
-            break
-
-        time.sleep(2)
+print(type(parser_text))
